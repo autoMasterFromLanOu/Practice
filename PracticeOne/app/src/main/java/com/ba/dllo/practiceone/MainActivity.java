80c134ac98c564ccf858bb427e1a43769236be33
巴为焱 <1160053654@qq.com>
2016-05-09 10:48:40 +0800

    第一次推送

---
+++ PracticeOne/app/src/main/java/com/ba/dllo/practiceone/MainActivity.java
@@ -0,0 +1,16 @@
+package com.ba.dllo.practiceone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * lallal
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
